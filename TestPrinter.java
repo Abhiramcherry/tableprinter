94999a8e703c29b017167ff93a1d16a21bbd219b
unknown <Abhiram>
2024-12-02 15:43:07 +0530

    Sample.java with content Abhiram

---
+++ TestPrinter.java
@@ -0,0 +1,9 @@
+public class TablePrinter {
+    public static void main(String[] args) {
+        System.out.println("Number\tSquare\tCube");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + "\t" + i * i + "\t" + i * i * i);
+        }
+    }
+}
+
